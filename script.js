e2293a3df7c3010be880c1736d61b647e8cb7007
MangoV3 <53501324+MangoV3@users.noreply.github.com>
2024-02-13 18:31:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('yesBtn').addEventListener('click', function() {
     if (event.target.id == 'yesBtn'){
-    window.location.href = 'https://en.wikipedia.org/wiki/Heart_symbol'; // Redirect to Wikipedia page if "Yes" button is clicked
+    window.location.href = 'https://en.wikipedia.org/wiki/Heart_symbol#/media/File:Heart_coraz%C3%B3n.svg'; // Redirect to Wikipedia page if "Yes" button is clicked
     }
 });
 
